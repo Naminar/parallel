8f3246abf797e614772545bb3bd555eac46dcc67
Naminar <buravko.va@phystech.edu>
2024-12-13 11:32:36 +0300

    Figures for each tasks

--- lab1/task1Plot.py
+++ lab1/task1Plot.py
@@ -16,33 +16,18 @@ def run_mpi():
         
 
 def make_plot(line_values, bar_values):
-    # plt.plot(data)
-    # plt.show()
-    # categories = ['A', 'B', 'C', 'D']
-    # bar_values = [3, 12, 5, 18]
-    # line_values = [1, 10, 6, 15]
-    x = np.arange(len(line_values))
+    x = np.arange(2, 7)
     fig, ax = plt.subplots()
-    ax.bar(x - 0.2, bar_values, width=0.4, label='Bar Values', color='b')
-    ax.plot(x + 0.2, line_values, label='Line Values', color='r', marker='o')
+    ax.bar(x - 0.2, bar_values, width=0.4, label='Serial execution', color='b')
+    ax.plot(x + 0.2, line_values, label='Parallel execution', color='r', marker='o')
 
     ax.set_xticks(x)
     # ax.set_xticklabels(categories)
-    ax.set_xlabel('Categories')
-    ax.set_ylabel('Values')
-    ax.set_title('Combined Bar and Line Plot')
+    ax.set_xlabel('Number of processes')
+    ax.set_ylabel('Time')
+    ax.set_title('Serial and Parallel execution')
     ax.legend()
     plt.show()
 
 if __name__ == "__main__":
-    # run_serial()
-    # run_omp()
-    # print(subprocess.run(['/usr/bin/mpiexec -np 2  ./mpi;'], capture_output=True, text=True).stdout)
     make_plot(run_mpi(), run_serial())
-#     import subprocess
-
-# # Correctly pass the command as a list of arguments
-#     result = subprocess.run(['/usr/bin/mpiexec', '-np', '2', './mpi'], capture_output=True, text=True)
-
-# # Print the output
-#     print(result.stdout)
